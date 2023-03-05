1896e6d43f1656c0f26ff8d46d00c9544873524d
emely3h <emely3h@gmail.com>
2023-03-05 23:54:15 +0100

    save images in between

--- prepare_data/split_to_tiles.py
+++ prepare_data/split_to_tiles.py
@@ -1,6 +1,10 @@
 import numpy as np
 from PIL import Image
 import PIL
+from dotenv import load_dotenv
+import os
+load_dotenv()
+data_path = os.environ.get('DATA_PATH')
 
 
 def fil_up_tile(tile, tile_size, third_dimension):
@@ -17,6 +21,7 @@ def split_image_into_tiles(array, tile_size, step_size):
     row_pixel, col_pixel, third_dim = array.shape
     row_index = 0
     print(f'Image shape: {array.shape}')
+    counter = 1
     while row_index < row_pixel:
         print(f'Current row index: {row_index}')
         col_index = 0
@@ -24,6 +29,11 @@ def split_image_into_tiles(array, tile_size, step_size):
             tile = array[row_index:row_index + tile_size, col_index:col_index + tile_size, :]
             tile = fil_up_tile(tile, tile_size, third_dim)
             result.append(tile.tolist())
+
+            img = Image.fromarray(np.uint8(tile.tolist()))
+            img.save(f'test_{counter}_{row_index}_{col_index}.png')
+            counter += 1
+
             col_index += step_size
         row_index += step_size
 
@@ -39,7 +49,8 @@ def image_tiles_array(img_path, tile_size, step_size):
     return tiles
 
 
-#test_x = image_tiles_array('../data/unflagged_rgb/2022_07_10/rgb.tif', 128, 90)
+test_x = image_tiles_array(f'{data_path}unflagged_rgb/2022_07_10/rgb.tif', 500, 490)
+#test = image_tiles_array('../data/test_image.jpeg', 100, 90)
 
 def create_mask_array(img_path):
     PIL.Image.MAX_IMAGE_PIXELS = 191357533
@@ -52,4 +63,9 @@ def create_mask_array(img_path):
             # if not 253, 255 or 0 pixel is valid otherwise invalid => what does 254 mean?
     return mask_array
 
-test_y = create_mask_array('../data/flags_applied/2022_07_10/wq.tif')
+#test_y = create_mask_array('../data/flags_applied/2022_07_10/wq.tif')
+
+# https://github.com/Devyanshu/image-split-with-overlap
+# test with saving in between
+# test with numpy strides
+# use other libraries => cv2, patchify, dask
