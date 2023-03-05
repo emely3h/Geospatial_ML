9d74b0acf6b2c72531dfea738ac9de16f8147527
emely3h <emely3h@gmail.com>
2023-03-05 21:16:42 +0100

    path as env

--- prepare_data/downsampling.py
+++ prepare_data/downsampling.py
@@ -1,16 +1,21 @@
 # Import the Images module from pillow
 from PIL import Image
+from dotenv import load_dotenv
+import os
+load_dotenv()
+data_path = os.environ.get('DATA_PATH')
 
-# Open the image by specifying the image path.
-image_path = "../data/test_image.jpg"
+image_path = f"{data_path}test_image.jpeg"
+
 image_file = Image.open(image_path)
 
+
 # the default
-image_file.save("data/test_image_downsized_1.jpg", quality=95)
+image_file.save(f"{data_path}test_image_downsized_1.jpeg", quality=95)
 
 # Changing the image resolution using quality parameter
 # Example-1
-image_file.save("data/test_image_downsized_2.jpg", quality=25)
+image_file.save(f"{data_path}test_image_downsized_2.jpeg", quality=25)
 
 # Example-2
-image_file.save("data/test_image_downsized_3.jpg", quality=1)
+image_file.save(f"{data_path}test_image_downsized_3.jpeg", quality=1)
