2ef3a36b561da2c57d00361d2103aeffd4d04685
Constantin Unterkofler <constantin.unterkofler@jvm.de>
2023-03-08 11:20:24 +0100

    feat(*): fix generate_patch function

--- prepare_data/prepare_folder.py
+++ prepare_data/prepare_folder.py
@@ -17,7 +17,8 @@ def rename_folders(dir_path):
             print(date)
 
             new_folder_name = dir_path + "/" + date
-            new_folder_path = os.path.join(os.path.dirname(folder), new_folder_name)
+            new_folder_path = os.path.join(
+                os.path.dirname(folder), new_folder_name)
 
             os.rename(old_path, new_folder_path)
 
@@ -60,17 +61,18 @@ def rename_files(folder_path):
 def generate_patch(img, patch_size, step):
     is3d = len(img.shape) == 3
     images = []
-    patch_shape = (patch_size, patch_size, 3) if is3d else (patch_size, patch_size)
+    patch_shape = (patch_size, patch_size, 3) if is3d else (
+        patch_size, patch_size)
     patches_img = patchify(img, patch_shape, step=step)
-    for i in range(patches_img.shape[0]):
-        for j in range(patches_img.shape[1]):
-            patch = patches_img[i, j, 0]
+    for row in range(patches_img.shape[0]):
+        for column in range(patches_img.shape[1]):
+            patch = patches_img[row, column, 0]
             patch = Image.fromarray(patch)
             images.append(
                 {
                     "patch": patch,
-                    "i": i,
-                    "j": j,
+                    "row": row,
+                    "column": column,
                 }
             )
     return images
@@ -86,30 +88,30 @@ def create_nested_dir(path, new_dir, where=2):
 
 # if the patch size is 256 and we want an overlap of 56, the step size would be 200
 def save_patches(
-    root_directory,
+    root,
     new_dir="patches",
     patch_size=256,
     step=200,
     max_image_pixels=933120000,
 ):
     Image.MAX_IMAGE_PIXELS = max_image_pixels
-    for path, _, files in os.walk(root_directory):
-        if new_dir in path:
-            return
+    # for path, _, files in os.walk(root_directory):
+    # if new_dir in path:
+    # return
 
-        dest_dir = create_nested_dir(path, new_dir, 2)
+    dest_dir = create_nested_dir(root, new_dir, 2)
 
-        for name in files:
-            if name.endswith("tif"):
-                file_path = os.path.join(path, name)
-                img = np.asarray(Image.open(file_path))
-                images = generate_patch(img, patch_size, step)
-                for image in images:
-                    patch = image["patch"]
+    for name in root:
+        if name.endswith("tif"):
+            file_path = os.path.join(root, name)
+            img = np.asarray(Image.open(file_path))
+            images = generate_patch(img, patch_size, step)
+            for image in images:
+                patch = image["patch"]
 
-                    i = image["i"]
-                    j = image["j"]
-                    patch_name = f"{name}_{i}_{j}.tif"
-                    patch_path = os.path.join(dest_dir, patch_name)
-                    patch.save(patch_path)
-                    print(f"Saved {patch_path}")
+                row = image["row"]
+                column = image["column"]
+                patch_name = f"{name}_{row}_{column}.tif"
+                patch_path = os.path.join(dest_dir, patch_name)
+                patch.save(patch_path)
+                print(f"Saved {patch_path}")
