c90076fc678f92d90a212f6941cdfc2ef4532c3e
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2023-03-29 14:22:16 +0200

    bugfix: Fix normalizing

--- experiments/helpers.py
+++ experiments/helpers.py
@@ -2,8 +2,8 @@ import numpy as np
 import tensorflow as tf
 
 def normalizing(X, y):
-  print(format('y shape: {}', y.shape))
+  print(format('y shape: {}', str(y.shape)))
   y_one_hot =  np.array([tf.one_hot(item, depth=3).numpy() for item in y])
-  print(format('y one hot shape: {}', y_one_hot.shape))
+  print(format('y one hot shape: {}', str(y_one_hot.shape)))
   X_normal = X/255
   return X_normal, y_one_hot
