41ead3449305bd40a9e20f2510a4db4aeaea113d
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2023-03-31 13:05:35 +0200

    feat: Add print functions again to normalizing

--- experiments/helpers.py
+++ experiments/helpers.py
@@ -1,6 +1,9 @@
 from tensorflow.keras.utils import to_categorical
 
 def normalizing(X, y):
+  print('y shape: ', y.shape)
+  print('Normalizing data...')
   y_one_hot = to_categorical(y, num_classes=3)
+  print('y one hot shape: ', y_one_hot.shape)
   X_normal = X/255
   return X_normal, y_one_hot
