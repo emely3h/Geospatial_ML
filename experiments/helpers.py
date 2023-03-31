fd0196d7bab141019b16d91738e3a725190c6d6a
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2023-03-31 10:47:33 +0200

    bugfix: Remove all print from normalizing

--- experiments/helpers.py
+++ experiments/helpers.py
@@ -2,8 +2,6 @@ import numpy as np
 import tensorflow as tf
 
 def normalizing(X, y):
-  print(y.shape)
   y_one_hot =  np.array([tf.one_hot(item, depth=3).numpy() for item in y])
-  print(y_one_hot.shape)
   X_normal = X/255
   return X_normal, y_one_hot
