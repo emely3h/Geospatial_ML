bugfix: Change normalizing to use only tensorflow

experiments/helpers.py
@@ -1,7 +1,6 @@
-import numpy as np
-import tensorflow as tf
+from tensorflow.keras.utils import to_categorical
 
 def normalizing(X, y):
-  y_one_hot =  np.array([tf.one_hot(item, depth=3).numpy() for item in y])
+  y_one_hot = to_categorical(y, num_classes=3)
   X_normal = X/255
   return X_normal, y_one_hot