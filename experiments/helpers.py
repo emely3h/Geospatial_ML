b96b138b6f279a3df5c464af28cee9051a4434ad
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2023-03-31 15:47:31 +0200

    feat: Create a refactor file for experiment 1

--- experiments/helpers.py
+++ experiments/helpers.py
@@ -7,3 +7,10 @@ def normalizing(X, y):
   print('y one hot shape: ', y_one_hot.shape)
   X_normal = X/255
   return X_normal, y_one_hot
+
+
+if __name__ == '__main__':
+  import numpy as np
+  X = np.random.rand(10, 32, 32, 3)
+  y = np.random.randint(0, 3, size=10)
+  X_normal, y_one_hot = normalizing(X, y)
