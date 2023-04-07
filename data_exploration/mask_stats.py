f09e6f3c145c462afc4a6aaff778927b33628581
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2023-04-07 12:26:46 +0200

    feat: Add slots to Mask_Stats

--- data_exploration/mask_stats.py
+++ data_exploration/mask_stats.py
@@ -16,29 +16,47 @@ class Mask_Stats:
         pix_invalid_per: percentage of invalid pixels
     """
 
-    def __init__(self, y_array):
+    __slots__ = [
+        "y_array",
+        "pix_land",
+        "pix_valid",
+        "pix_invalid",
+        "sum_pix",
+        "pix_land_per",
+        "pix_valid_per",
+        "pix_invalid_per",
+    ]
+
+    def __init__(self, y_array: np.ndarray):
         self.y_array = y_array
+        self.pix_land: int = self.num_of_pixels_per_class(y_array, 2)
+        self.pix_valid: int = self.num_of_pixels_per_class(y_array, 1)
+        self.pix_invalid: int = self.num_of_pixels_per_class(y_array, 0)
+        self.sum_pix: int = self.pix_land + self.pix_valid + self.pix_invalid
 
-        self.pix_land = self.num_of_pixels_per_class(y_array, 2)
-        self.pix_valid = self.num_of_pixels_per_class(y_array, 1)
-        self.pix_invalid = self.num_of_pixels_per_class(y_array, 0)
-        self.sum_pix = self.pix_land + self.pix_valid + self.pix_invalid
+        self.pix_land_per: float = 100 / self.sum_pix * self.pix_land
+        self.pix_valid_per: float = 100 / self.sum_pix * self.pix_valid
+        self.pix_invalid_per: float = 100 / self.sum_pix * self.pix_invalid
 
-        self.pix_land_per = 100 / self.sum_pix * self.pix_land
-        self.pix_valid_per = 100 / self.sum_pix * self.pix_valid
-        self.pix_invalid_per = 100 / self.sum_pix * self.pix_invalid
-        #self.print_stats()
-
-    def num_of_pixels_per_class(self, y_mask, label):
+    def num_of_pixels_per_class(self, y_mask: any, label: any) -> int:
         flatten = np.reshape(y_mask, (-1,))
-        pixel_match = (flatten == label)
+        pixel_match = flatten == label
         pix_per_class = np.count_nonzero(pixel_match)
         return pix_per_class
 
-    def print_stats(self):
-        print(f'Shape: {self.y_array.shape}')
-        print(f'Land pixels: {self.pix_land}  {self.pix_land_per:.3f} %')
-        print(f'Valid pixels: {self.pix_valid}  {self.pix_valid_per:.3f} %')
-        print(f'Invalid pixels: {self.pix_invalid}  {self.pix_invalid_per:.3f} %')
+    def print_stats(self) -> None:
+        print(f"Shape: {self.y_array.shape}")
+        print(f"Land pixels: {self.pix_land}  {self.pix_land_per:.3f} %")
+        print(f"Valid pixels: {self.pix_valid}  {self.pix_valid_per:.3f} %")
+        print(f"Invalid pixels: {self.pix_invalid}  {self.pix_invalid_per:.3f} %")
         # print("Class distribution:", np.bincount(y_array))
-        print(f'Sum: {(self.pix_land + self.pix_valid + self.pix_invalid) // 256 // 256}')
+        print(
+            f"Sum: {(self.pix_land + self.pix_valid + self.pix_invalid) // 256 // 256}"
+        )
+
+
+if __name__ == "__main__":
+    # 12 sec -> 4sec
+    y = np.zeros((6661, 256, 256), dtype=np.float32)
+    mask_stats = Mask_Stats(y)
+    mask_stats.print_stats()
